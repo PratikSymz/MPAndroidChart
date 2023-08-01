30cb5a8236726d98919fe8d8aac154f0ee50f389
PratikSymz <budhiraja.p@northeastern.edu>
2023-08-01 13:28:42 -0400

    Added custom circular highlight functionality

--- MPChartLib/src/main/java/com/github/mikephil/charting/interfaces/datasets/ILineScatterCandleRadarDataSet.java
+++ MPChartLib/src/main/java/com/github/mikephil/charting/interfaces/datasets/ILineScatterCandleRadarDataSet.java
@@ -9,27 +9,52 @@ import com.github.mikephil.charting.data.Entry;
  */
 public interface ILineScatterCandleRadarDataSet<T extends Entry> extends IBarLineScatterCandleBubbleDataSet<T> {
 
-    /**
-     * Returns true if vertical highlight indicator lines are enabled (drawn)
-     * @return
-     */
-    boolean isVerticalHighlightIndicatorEnabled();
+  /**
+   * Returns true if vertical highlight indicator lines are enabled (drawn)
+   *
+   * @return
+   */
+  boolean isVerticalHighlightIndicatorEnabled();
 
-    /**
-     * Returns true if vertical highlight indicator lines are enabled (drawn)
-     * @return
-     */
-    boolean isHorizontalHighlightIndicatorEnabled();
+  /**
+   * Returns true if vertical highlight indicator lines are enabled (drawn)
+   *
+   * @return
+   */
+  boolean isHorizontalHighlightIndicatorEnabled();
 
-    /**
-     * Returns the line-width in which highlight lines are to be drawn.
-     * @return
-     */
-    float getHighlightLineWidth();
+  /**
+   * Returns true if circular highlight indicator is enabled (drawn)
+   *
+   * @return
+   */
+  boolean isCircularHighlightIndicatorEnabled();
 
-    /**
-     * Returns the DashPathEffect that is used for highlighting.
-     * @return
-     */
-    DashPathEffect getDashPathEffectHighlight();
+  /**
+   * Returns the radii used to draw the circular highlight lines
+   *
+   * @return
+   */
+  float[] getCircleHighlightRadii();
+
+  /**
+   * Returns the colors used to draw the circular highlight lines
+   *
+   * @return
+   */
+  int[] getCircleHighlightColors();
+
+  /**
+   * Returns the line-width in which highlight lines are to be drawn.
+   *
+   * @return
+   */
+  float getHighlightLineWidth();
+
+  /**
+   * Returns the DashPathEffect that is used for highlighting.
+   *
+   * @return
+   */
+  DashPathEffect getDashPathEffectHighlight();
 }
--- MPChartLib/src/main/java/com/github/mikephil/charting/renderer/LineScatterCandleRadarRenderer.java
+++ MPChartLib/src/main/java/com/github/mikephil/charting/renderer/LineScatterCandleRadarRenderer.java
@@ -1,10 +1,13 @@
 package com.github.mikephil.charting.renderer;
 
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
 import android.graphics.Path;
 
 import com.github.mikephil.charting.animation.ChartAnimator;
 import com.github.mikephil.charting.interfaces.datasets.ILineScatterCandleRadarDataSet;
+import com.github.mikephil.charting.utils.Utils;
 import com.github.mikephil.charting.utils.ViewPortHandler;
 
 /**
@@ -12,52 +15,65 @@ import com.github.mikephil.charting.utils.ViewPortHandler;
  */
 public abstract class LineScatterCandleRadarRenderer extends BarLineScatterCandleBubbleRenderer {
 
-    /**
-     * path that is used for drawing highlight-lines (drawLines(...) cannot be used because of dashes)
-     */
-    private Path mHighlightLinePath = new Path();
+  /**
+   * path that is used for drawing highlight-lines (drawLines(...) cannot be used because of dashes)
+   */
+  private Path mHighlightLinePath = new Path();
 
-    public LineScatterCandleRadarRenderer(ChartAnimator animator, ViewPortHandler viewPortHandler) {
-        super(animator, viewPortHandler);
+  public LineScatterCandleRadarRenderer(ChartAnimator animator, ViewPortHandler viewPortHandler) {
+    super(animator, viewPortHandler);
+  }
+
+  /**
+   * Draws vertical & horizontal highlight-lines if enabled.
+   *
+   * @param c
+   * @param x   x-position of the highlight line intersection
+   * @param y   y-position of the highlight line intersection
+   * @param set the currently drawn dataset
+   */
+  protected void drawHighlightLines(Canvas c, float x, float y, ILineScatterCandleRadarDataSet set) {
+
+    // set color and stroke-width
+    mHighlightPaint.setColor(set.getHighLightColor());
+    mHighlightPaint.setStrokeWidth(set.getHighlightLineWidth());
+
+    // draw highlighted lines (if enabled)
+    mHighlightPaint.setPathEffect(set.getDashPathEffectHighlight());
+
+    // draw vertical highlight lines
+    if (set.isVerticalHighlightIndicatorEnabled()) {
+
+      // create vertical path
+      mHighlightLinePath.reset();
+      mHighlightLinePath.moveTo(x, mViewPortHandler.contentTop());
+      mHighlightLinePath.lineTo(x, mViewPortHandler.contentBottom());
+
+      c.drawPath(mHighlightLinePath, mHighlightPaint);
     }
 
-    /**
-     * Draws vertical & horizontal highlight-lines if enabled.
-     *
-     * @param c
-     * @param x x-position of the highlight line intersection
-     * @param y y-position of the highlight line intersection
-     * @param set the currently drawn dataset
-     */
-    protected void drawHighlightLines(Canvas c, float x, float y, ILineScatterCandleRadarDataSet set) {
+    // draw horizontal highlight lines
+    if (set.isHorizontalHighlightIndicatorEnabled()) {
 
-        // set color and stroke-width
-        mHighlightPaint.setColor(set.getHighLightColor());
-        mHighlightPaint.setStrokeWidth(set.getHighlightLineWidth());
+      // create horizontal path
+      mHighlightLinePath.reset();
+      mHighlightLinePath.moveTo(mViewPortHandler.contentLeft(), y);
+      mHighlightLinePath.lineTo(mViewPortHandler.contentRight(), y);
 
-        // draw highlighted lines (if enabled)
-        mHighlightPaint.setPathEffect(set.getDashPathEffectHighlight());
-
-        // draw vertical highlight lines
-        if (set.isVerticalHighlightIndicatorEnabled()) {
-
-            // create vertical path
-            mHighlightLinePath.reset();
-            mHighlightLinePath.moveTo(x, mViewPortHandler.contentTop());
-            mHighlightLinePath.lineTo(x, mViewPortHandler.contentBottom());
-
-            c.drawPath(mHighlightLinePath, mHighlightPaint);
-        }
-
-        // draw horizontal highlight lines
-        if (set.isHorizontalHighlightIndicatorEnabled()) {
-
-            // create horizontal path
-            mHighlightLinePath.reset();
-            mHighlightLinePath.moveTo(mViewPortHandler.contentLeft(), y);
-            mHighlightLinePath.lineTo(mViewPortHandler.contentRight(), y);
-
-            c.drawPath(mHighlightLinePath, mHighlightPaint);
-        }
+      c.drawPath(mHighlightLinePath, mHighlightPaint);
     }
+
+    // draw circular highlight lines
+    if (set.isCircularHighlightIndicatorEnabled()) {
+      float[] circleHighlightRadii = set.getCircleHighlightRadii();
+      int[] circleHighlightColors = set.getCircleHighlightColors();
+
+      for (int i = circleHighlightColors.length - 1; i >= 0; i--) {
+        Paint circlePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        circlePaint.setColor(circleHighlightColors[i]);
+        circlePaint.setStyle(Style.FILL);
+        c.drawCircle(x, y, Utils.convertDpToPixel(circleHighlightRadii[i]), circlePaint);
+      }
+    }
+  }
 }
